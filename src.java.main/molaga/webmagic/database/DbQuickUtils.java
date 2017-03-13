08a35695833c4f9920f388ffabb12305f24c923c
zhou <zhou@molaga.world>
2017-03-14 00:32:21 +0800

    connect to qcloud

--- src.java.main/molaga/webmagic/database/DbQuickUtils.java
+++ src.java.main/molaga/webmagic/database/DbQuickUtils.java
@@ -1,9 +1,14 @@
 package molaga.webmagic.database;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.Set;
 
 import org.junit.Test;
@@ -12,16 +17,35 @@ import org.slf4j.LoggerFactory;
 
 public class DbQuickUtils {
 	private static Logger logger = LoggerFactory.getLogger(DbQuickUtils.class);
+	private static Properties props = new Properties();
+	static String path;
+	static {
+		try {
+			path = DbQuickUtils.class.getResource("/").toURI()
+					.getPath()
+					+ "jdbc.properties";
+			props.load(new FileInputStream(path));
+
+		} catch (URISyntaxException e) {
+			logger.error("file path [{}] URISyntaxException .", path);
+		} catch (FileNotFoundException e) {
+			logger.error("jdbc.properties [{}] is not found.", path);
+		} catch (IOException e) {
+			logger.error("error occurs while reading jdbc.properties file [{}].", path);
+		}
+
+	}
 
 	@Test
 	public void getConnection() {
-		String url = "jdbc:mysql://localhost:3306/acfun";
-		String username = "zhou";
-		String password = "zhou";
+		String url = props.getProperty("jdbc.mysql.uri");
+		String username = props.getProperty("jdbc.mysql.user");
+		String password = props.getProperty("jdbc.mysql.passwd");
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 
-			Connection con = DriverManager.getConnection(url, username, password);
+			Connection con = DriverManager.getConnection(url, username,
+					password);
 			logger.debug("数据库联接成功...");
 			Set<String> props = con.getClientInfo().stringPropertyNames();
 			for (String s : props)
